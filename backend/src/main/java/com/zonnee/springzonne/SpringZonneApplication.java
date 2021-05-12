71eaa8fdac1335f74bea8b32d669e2a3daac37d1
Joao Paulo <jojolimaj23@gmail.com>
2021-05-12 20:39:14 -0300

    Project Created

---
+++ backend/src/main/java/com/zonnee/springzonne/SpringZonneApplication.java
@@ -0,0 +1,13 @@
+package com.zonnee.springzonne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZonneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZonneApplication.class, args);
+	}
+
+}
